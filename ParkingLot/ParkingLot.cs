c17bd6b9ffd40baaab3306395717325badbb63c9
Yiyi Han <YHan18@slb.com>
2023-11-03 16:07:43 +0800

    feat: add the park and fetch method

---
+++ ParkingLot/ParkingLot.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkingLotManagement
+{
+    public class ParkingLot
+    {
+        private string car;
+        public string Fetch(string ticket)
+        {
+            return car;
+        }
+
+        public string Park(string car)
+        {
+            this.car = car;
+            return "q";
+        }
+    }
+}
---
+++ ParkingLotTest/ParkingLotTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using ParkingLotManagement;
+
+namespace ParkingLotTest
+{
+    public class ParkingLotTest
+    {
+        [Fact]
+        public void Should_get_the_same_car_when_fetch_car_by_ticket()
+        {
+            ParkingLot parkingLot = new ParkingLot();
+            string ticket = parkingLot.Park("car");
+
+            string car = parkingLot.Fetch(ticket);
+
+            Assert.Equal("car", car);
+        }
+    }
+}
