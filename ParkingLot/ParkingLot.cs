3b1d2727867db0b31de388be16ac29649eb6ec08
Han Xudong <xhan14@slb.com>
2023-11-03 16:22:27 +0800

    feat: add function "GetCar"

--- ParkingLot/ParkingLot.cs
+++ ParkingLot/ParkingLot.cs
@@ -6,10 +6,23 @@
         private string insideCar;
         private string ticket;
 
+        public string GetCar(string ticket)
+        {
+            if (ticket == this.ticket)
+            {
+                return insideCar;
+            }
+            else
+            {
+                return "wrong ticket";
+            }
+        }
+
         public string Park(string car)
         {
             insideCar = car;
-            return "q";
+            ticket = "q";
+            return ticket;
         }
     }
 }
--- ParkingLotTest/ParkingLotTest.cs
+++ ParkingLotTest/ParkingLotTest.cs
@@ -6,11 +6,24 @@ namespace ParkingLotTest
     public class ParkingLotTest
     {
         [Fact]
-        public void Should_return_ticket_when_parking_in_a_parkinglot_given_a_car()
+        public void Should_return_ticket_when_park_in_a_parkinglot_given_a_car()
         {
             ParkingLot parking = new ParkingLot();
             string ticket = parking.Park("car");
             Assert.Equal("q", ticket);
+        }
+
+        [Fact]
+        public void Should_return_correct_car_when_get_car_in_a_parkinglot_given_a_ticket()
+        {
+            ParkingLot parking = new ParkingLot();
+            string ticket = parking.Park("car");
+
+            string result = parking.GetCar(ticket);
+            string result2 = parking.GetCar("x");
+
+            Assert.Equal("car", result);
+            Assert.Equal("wrong ticket", result2);
         }
     }
 }
