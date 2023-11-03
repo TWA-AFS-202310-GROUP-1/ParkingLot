b446e513841ac3caf807262ac9bf3e30a5be5e08
Xinxin Xu <xxu37@slb.com>
2023-11-03 18:14:52 +0800

    feat: should get a ticket when standard parking boy park a car

---
+++ ParkingLot/StandardParkingBoy.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day5
+{
+    public class StandardParkingBoy
+    {
+        private ParkingLot parkingLot;
+
+        public StandardParkingBoy(ParkingLot parkingLot)
+        {
+            this.parkingLot = parkingLot;
+        }
+
+        public string Park(string car)
+        {
+            return this.parkingLot.Park(car);
+        }
+    }
+}
---
+++ ParkingLotTest/StandardParkingBoyTest.cs
@@ -0,0 +1,24 @@
+﻿using Day5;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ParkingLotTest
+{
+    public class StandardParkingBoyTest
+    {
+        [Fact]
+        public void Should_get_a_ticket_when_park_given_car()
+        {
+            ParkingLot parkingLot = new ParkingLot();
+            StandardParkingBoy boy = new StandardParkingBoy(parkingLot);
+
+            string ticket = boy.Park("car1");
+
+            Assert.Equal("T-car1", ticket);
+        }
+    }
+}
