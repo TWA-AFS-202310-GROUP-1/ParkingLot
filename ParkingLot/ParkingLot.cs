f0a18b8b5634caacc7ebdd814d0bce48f7eb9906
Wency3 <XWen3@slb.com>
2023-11-03 16:07:50 +0800

    feat: complete the function of get the same car when fetch car by ticket.

---
+++ ParkingLot/ParkingLot.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day5
+{
+    public class ParkingLot
+    {
+        private string car;
+        //private Dictionary<string, string> ticket2car = new Dictionary<string, string>();
+        public string Fetch(string ticket)
+        {
+            return car;
+            //ticket2car[ticket] = car;
+        }
+
+        public string Park(string car)
+        {
+            this.car = car;
+/*            string ticket = "T-" + car;
+            ticket2car.Add(ticket, car);*/
+            return "ticket";
+        }
+    }
+}
---
+++ ParkingLotTest/ParkingLotTest.cs
@@ -0,0 +1,21 @@
+﻿using Day5;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+namespace ParkingLotTest
+{
+    public class ParkingLotTest
+    {
+        [Fact]
+        public void Should_get_the_same_car_when_fetch_car_by_ticket()
+        {
+            ParkingLot parkingLot = new ParkingLot();
+            string ticket = parkingLot.Park("car");
+            string car = parkingLot.Fetch(ticket);
+            Assert.Equal("car", car);
+        }
+    }
+}
