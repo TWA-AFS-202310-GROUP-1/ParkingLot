b164fcb5ad94f28594d81cda3044ed744c12982d
Xinxin Xu <xxu37@slb.com>
2023-11-03 16:08:43 +0800

    feat: add funtion for should get the same car when fetch car given ticket

---
+++ ParkingLot/ParkingLot.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day5
+{
+    public class ParkingLot
+    {
+        private string car;
+
+        public string Fetch(string ticket)
+        {
+            return car;
+        }
+
+        public string Park(string car)
+        {
+            this.car = car;
+            return "ticket";
+        }
+    }
+}
---
+++ ParkingLotTest/ParkingLotTest.cs
@@ -0,0 +1,24 @@
+﻿using Day5;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ParkingLotTest
+{
+    public class ParkingLotTest
+    {
+        [Fact]
+        public void Should_get_the_same_car_when_fetch_car_given_ticket()
+        {
+            ParkingLot parkingLot = new ParkingLot();
+            string ticket = parkingLot.Park("car");
+
+            string car = parkingLot.Fetch(ticket);
+
+            Assert.Equal("car", car);
+        }
+    }
+}
