0bf5688cbdfe9340bfaf5e66f96cc11e3709db34
Yiyi Han <YHan18@slb.com>
2023-11-04 14:49:01 +0800

    test: add a method of fetching right car by ticket in ParkingBoy class

--- ParkingLotTest/ParkingBoyTest.cs
+++ ParkingLotTest/ParkingBoyTest.cs
@@ -25,5 +25,24 @@ namespace ParkingLotTest
             //Then
             Assert.Equal("car", car);
         }
+
+        [Theory]
+        [InlineData("car1", "car2")]
+        public void Should_get_the_right_car_when_fetch_car_by_ticket(string carName1, string carName2)
+        {
+            //Given
+            ParkingLot parkingLot = new ParkingLot();
+            ParkingBoy parkingBoy = new ParkingBoy(parkingLot);
+
+            //When
+            string ticket1 = parkingBoy.Park(carName1);
+            string ticket2 = parkingBoy.Park(carName2);
+            string car1 = parkingBoy.Fetch(ticket1);
+            string car2 = parkingBoy.Fetch(ticket2);
+
+            //Then
+            Assert.Equal("car1", car1);
+            Assert.Equal("car2", car2);
+        }
     }
 }
