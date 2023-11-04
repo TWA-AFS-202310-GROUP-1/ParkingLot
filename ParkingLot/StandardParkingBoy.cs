5f5e5e295963c1bae15be500bac1d27f51d47765
Han Xudong <xhan14@slb.com>
2023-11-04 18:42:10 +0800

    feat: add 'StandardParkingBoy' with park and fetch functions

---
+++ ParkingLot/StandardParkingBoy.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkingLot
+{
+    public class StandardParkingBoy
+    {
+        private ParkingLot parkingLot;
+        public StandardParkingBoy()
+        {
+            parkingLot = new ParkingLot();
+        }
+
+        public string Park(string car)
+        {
+            return parkingLot.Park(car);
+        }
+
+        public string Fetch(string ticket)
+        {
+            return parkingLot.FetchCar(ticket);
+        }
+    }
+}
---
+++ ParkingLotTest/ParkingBoyTest.cs
@@ -0,0 +1,41 @@
+﻿namespace ParkingLotTest
+{
+    using ParkingLot;
+    using System.Reflection;
+    using Xunit;
+    using Xunit.Sdk;
+
+    public class ParkingBoyTest
+    {
+        [Fact]
+        public void Should_return_a_ticket_when_park_car_by_parking_boy_given_a_car()
+        {
+            //given
+            StandardParkingBoy standardParkingBoy = new StandardParkingBoy();
+
+            //when
+            string ticket = standardParkingBoy.Park("car");
+
+            //then
+            Assert.Equal("-car", ticket);
+        }
+
+        [Fact]
+        public void Should_return_correct_result_when_fetch_car_by_parking_boy_given_a_ticket()
+        {
+            //given
+            StandardParkingBoy standardParkingBoy = new StandardParkingBoy();
+            string ticket = standardParkingBoy.Park("car");
+            string ticket2 = standardParkingBoy.Park("car");
+
+            //when
+            string fetchedCar = standardParkingBoy.Fetch(ticket);
+
+            //then
+            Assert.Equal("car", fetchedCar);
+            Assert.Throws<UnvalidTicketException>(() => standardParkingBoy.Fetch(ticket));
+            Assert.Throws<UnvalidTicketException>(() => standardParkingBoy.Fetch(null));
+            Assert.Throws<UnvalidTicketException>(() => standardParkingBoy.Fetch("x"));
+        }
+    }
+}
