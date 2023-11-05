b6fb9b5fff0700ecbe41d2d320b5c3c8e476ba35
Tianwei Liu <TLiu25@slb.com>
2023-11-05 13:04:27 +0800

    feat: story4 parking boy has multiple parking lots

---
+++ ParkingLot/ParkingBoyMultipleLots.cs
@@ -0,0 +1,60 @@
+﻿using ParkingLotSystem;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkingLotSystem
+{
+    public class ParkingBoyMultipleLots
+    {
+        private readonly List<ParkingLot> _parkingLots;
+        public List<ParkingLot> ParkingLots { get => _parkingLots; }
+
+        public ParkingBoyMultipleLots(List<ParkingLot> parkingLots)
+        {
+            _parkingLots = parkingLots;
+        }
+
+        public string Park(string car)
+        {
+            foreach (var parkingLot in _parkingLots)
+            {
+                try
+                {
+                    return parkingLot.Park(car);
+                }
+                catch (Exception ex)
+                {
+                    if (ex is NoPositionException)
+                    {
+                        continue;
+                    }
+                }
+            }
+
+            throw new NoPositionException("No available position.");
+        }
+
+        public string Fetch(string ticket)
+        {
+            foreach (var parkingLot in _parkingLots)
+            {
+                try
+                {
+                    return parkingLot.Fetch(ticket);
+                }
+                catch (Exception ex)
+                {
+                    if (ex is WrongTicketException)
+                    {
+                        continue;
+                    }
+                }
+            }
+
+            throw new WrongTicketException("Unrecognized parking ticket.");
+        }
+    }
+}
