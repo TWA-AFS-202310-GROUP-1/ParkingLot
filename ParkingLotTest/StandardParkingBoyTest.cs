9851c3c35b15799034bfbc6b80ac7e274688d5b8
Xinxin Xu <xxu37@slb.com>
2023-11-05 10:28:12 +0800

    test: add test for standard parking boy should return nothing with error message when fetch car given wrong ticket

--- ParkingLotTest/StandardParkingBoyTest.cs
+++ ParkingLotTest/StandardParkingBoyTest.cs
@@ -47,5 +47,16 @@ namespace ParkingLotTest
             Assert.Equal("car1", car1);
             Assert.Equal("car2", car2);
         }
+
+        [Fact]
+        public void Should_return_nothing_with_error_message_when_fetch_car_given_wrong_ticket()
+        {
+            ParkingLot parkingLot = new ParkingLot();
+            StandardParkingBoy boy = new StandardParkingBoy(parkingLot);
+            string ticket1 = boy.Park("car1");
+
+            WrongTicketException wrongTicketException = Assert.Throws<WrongTicketException>(() => boy.Fetch("WRONG"));
+            Assert.Equal("Unrecognized parking ticket", wrongTicketException.Message);
+        }
     }
 }
