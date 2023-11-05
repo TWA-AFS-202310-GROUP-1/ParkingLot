f2d7007530de477c138c98fb78de22462d44d77b
Tianwei Liu <TLiu25@slb.com>
2023-11-05 14:29:49 +0800

    feat: story6 strategy pattern

---
+++ ParkingLot/Strategy/ParkingStrategy.cs
@@ -0,0 +1,43 @@
+﻿using ParkingLotSystem;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkingLotSystem.Strategy
+{
+    public interface IParkingStrategy
+    {
+         string Park(List<ParkingLot> parkingLots, string car);
+    }
+
+    public class StandardParkingStrategy : IParkingStrategy
+    {
+        public string Park(List<ParkingLot> parkingLots, string car)
+        {
+            foreach (var parkingLot in parkingLots)
+            {
+                try
+                {
+                    return parkingLot.Park(car);
+                }
+                catch (NoPositionException)
+                {
+                    continue;
+                }
+            }
+
+            throw new NoPositionException("No available position.");
+        }
+    }
+
+    public class SmartParkingStrategy : IParkingStrategy
+    {
+        public string Park(List<ParkingLot> parkingLots, string car)
+        {
+            var parkingLot = parkingLots.OrderByDescending(lot => lot.AvailableCapacity).First();
+            return parkingLot.Park(car);
+        }
+    }
+}
