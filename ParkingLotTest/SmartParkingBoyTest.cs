2c971d1f931291ed55312c6767c5f600c1e2db66
Xinxin Xu <xxu37@slb.com>
2023-11-05 12:59:47 +0800

    test: add test for smart parking boy with two fully occupied parkinglots when park should return nothing with error message

--- ParkingLotTest/SmartParkingBoyTest.cs
+++ ParkingLotTest/SmartParkingBoyTest.cs
@@ -99,5 +99,25 @@ namespace ParkingLotTest
             WrongTicketException wrongTicketException = Assert.Throws<WrongTicketException>(() => boy.Fetch(ticket1));
             Assert.Equal("Unrecognized parking ticket", wrongTicketException.Message);
         }
+
+        [Fact]
+        public void Should_return_nothing_with_error_message_when_park_given_two_parkinglots_fully_occupied()
+        {
+            List<ParkingLot> parkingLots = new List<ParkingLot>();
+            for (int i = 0; i < 2; i++)
+            {
+                parkingLots.Add(new ParkingLot());
+            }
+
+            SmartParkingBoy boy = new SmartParkingBoy(parkingLots);
+
+            for (int i = 0; i < 20; i++)
+            {
+                boy.Park("car" + (i + 1).ToString());
+            }
+
+            NoPositionException noPositionException = Assert.Throws<NoPositionException>(() => boy.Park("car21"));
+            Assert.Equal("No available position.", noPositionException.Message);
+        }
     }
 }
